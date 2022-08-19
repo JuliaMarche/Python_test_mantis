c7a9d2d1a919ca28941bd000c2ec556530c66ee6
JuliaMarche <yuliamarche@gmail.com>
2022-08-19 08:54:08 +0300

    Исправление теста

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -3,7 +3,7 @@ from model.project import Project
 def test_add_project(app, db, json_projects):
     project = json_projects
     old_projects = db.get_project_list()
-    app.project.fill_form(project)
+    app.project.add_project(project)
     new_projects = db.get_project_list()
     old_projects.append(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
